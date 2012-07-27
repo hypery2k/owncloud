b057ea309107de23316dbfc184b69ffa33d35541
Martin Reinhardt <Martin.Reinhardt@adesso.de>
2012-07-27 10:25:33 +0200

    fix some layout issues on IE and FF, see issue#22

--- apps/roundcube/src/lib/mail.php
+++ apps/roundcube/src/lib/mail.php
@@ -170,6 +170,11 @@ class OC_RoundCube_App {
 					echo '
 				    height = (height < 0) ? 0 : height;
 				    document.getElementById(\'roundcubeFrame\').style.height = height + \'px\';
+					// fix scrollbar issue
+					$(\'#content\').css(\'overflow\',\'hidden\');
+					$(\'#content\').css(\'height\',\'+ height+\');
+					width=$(\'#content\').css(\'width\');
+					$(\'#content\').css(\'width\',\'+ width +\');
 					
 				}
 	
@@ -177,29 +182,36 @@ class OC_RoundCube_App {
 					resizeIframe();
 
 					var mainscreen = $(\'#roundcubeFrame\').contents().find(\'#mainscreen\');
-					// remove header line, includes about line and 
-					var top_line = $(\'#roundcubeFrame\').contents().find(\'#topline\');
-					// correct top padding
-					var top_margin= 5;
-					try{
-						top_margin= parseInt(mainscreen.css(\'top\'),10)-top_line.height();
-					} catch(e){}
-					top_line.remove();
+                    // remove header line, includes about line and
+                    var top_line = $(\'#roundcubeFrame\').contents().find(\'#topline\');
+                    // correct top padding
+                    var top_margin=10;
+                    try{    
+                            var top_nav = $(\'#roundcubeFrame\').contents().find(\'#topnav\');
+                            // check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
+                            // TODO refactor and move theme check
+                            if(top_nav.height()!=null){
+                                    top_margin= 10;
+                            } else {
+                                    top_margin= parseInt(mainscreen.css(\'top\'),10)-top_line.height();
+                            }
+                    } catch (e) {}
+                    top_line.remove();
 
-					// remove logout button	 				
-					$(\'#roundcubeFrame\').contents().find(\'.button-logout\').remove();
-	
-					if('.$disable_header_nav.') {
-						
-						var top_nav = $(\'#roundcubeFrame\').contents().find(\'#topnav\');
-						// check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
-						if(top_nav.height()==null){
-							top_nav = $(\'#roundcubeFrame\').contents().find(\'#taskbar\');
-						} else {
-							top_margin= top_margin-top_nav.height();
-						}
-						top_nav.remove();
-					}
+                    // remove logout button
+                    $(\'#roundcubeFrame\').contents().find(\'.button-logout\').remove();
+
+                    if('.$disable_header_nav.') {
+
+                            var top_nav = $(\'#roundcubeFrame\').contents().find(\'#topnav\');
+                            // check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
+                            if(top_nav.height()==null){
+                                    top_nav = $(\'#roundcubeFrame\').contents().find(\'#taskbar\');
+                            } else {
+                                    //top_in= top_margin-top_nav.height();
+                            }
+                            top_nav.remove();
+                    }
 					// correct top padding
 					$(\'#roundcubeFrame\').contents().find(\'#mainscreen\').css(\'top\',top_margin);
 
