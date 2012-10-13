4a565e68d5165ccfaae849fa3808b07220534fdc
Martin Reinhardt <Martin.Reinhardt@adesso.de>
2012-10-13 09:52:25 +0200

    added workaround for app register
    fixes issue #48

--- apps/roundcube/src/appinfo/app.php
+++ apps/roundcube/src/appinfo/app.php
@@ -1,43 +1,51 @@
 <?php
 /**
-* ownCloud - roundcube mail plugin
-*
-\* @author Martin Reinhardt and David Jaedke
-* @copyright 2012 Martin Reinhardt contact@martinreinhardt-online.de
-* 
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
-* License as published by the Free Software Foundation; either 
-* version 3 of the License, or any later version.
-* 
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
-*  
-* You should have received a copy of the GNU Lesser General Public 
-* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
-* 
-*/
+ * ownCloud - roundcube mail plugin
+ *
+ \* @author Martin Reinhardt and David Jaedke
+ * @copyright 2012 Martin Reinhardt contact@martinreinhardt-online.de
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-$l=new OC_L10N('roundcube');
+$l = new OC_L10N('roundcube');
 OC::$CLASSPATH['OC_RoundCube_App'] = 'apps/roundcube/lib/mail.php';
 OC::$CLASSPATH['RoundcubeLogin'] = 'apps/roundcube/lib/RoundcubeLogin.class.php';
 
-OC_APP::registerAdmin('roundcube','adminSettings');
-OC_App::registerPersonal('roundcube','userSettings');
+OC_APP::registerAdmin('roundcube', 'adminSettings');
+OC_App::registerPersonal('roundcube', 'userSettings');
 
-OC_App::register( array(
-	'order' => 10,
-	'id' => 'roundcube',
-	'name' => 'RoundCube Mail' )
-);
+// needed for owncloud version <= 4.5
+try {
+	OC_App::register(
+		array(
+			'order' => 10, 
+			'id' => 'roundcube', 
+			'name' => 'RoundCube Mail'
+		)
+	);
+} catch (Exception $e) {
+}
 
-OC_App::addNavigationEntry( array(
-    'id' => 'roundcube_index',
-    'order' => 10,
-    'href' => OC_Helper::linkTo( 'roundcube', 'index.php' ),
-    'icon' => OC_Helper::imagePath( 'roundcube', 'mail.png' ),
-    'name' => $l->t('Webmail'))
+OC_App::addNavigationEntry(
+	array(
+		'id' => 'roundcube_index', 
+		'order' => 10, 
+		'href' => OC_Helper::linkTo('roundcube', 'index.php'), 
+		'icon' => OC_Helper::imagePath('roundcube', 'mail.png'), 
+		'name' => $l -> t('Webmail')
+	)
 );
 ?>
