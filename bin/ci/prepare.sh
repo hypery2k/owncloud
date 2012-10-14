c3f00f3d6f415ad8bf25eb652f56abc69dde105a
Martin Reinhardt <Martin.Reinhardt@adesso.de>
2012-10-14 23:49:39 +0200

    Fix relative path error in ci script
    see issue #46

--- bin/ci/prepare.sh
+++ bin/ci/prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 cd bin
+
+# Prepare Selenium
 mkdir sausage && cd sausage
 curl -s https://raw.github.com/jlipps/sausage-bun/master/givememysausage.php | SAUCE_USERNAME=mreinhardt SAUCE_ACCESS_KEY=28125823-d5a7-4ffe-a29b-f2976cb8d473 php
+cd ..
+
+cd ..
 # SETUP OWNCLOUD
 sudo cp -rp travis_ci/owncloud_releases/4.0.8 /var/www/mysql/4.0.8
 sudo cp -rp travis_ci/owncloud_releases/4.0.8 /var/www/sqllite/4.0.8
