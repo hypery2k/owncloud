2e1d6b2ff86f37610fd2d793ccf902cf1059e043
Martin Reinhardt <Martin.Reinhardt@adesso.de>
2013-01-07 07:40:03 +0100

    Fixing layout issues
    see issue #88

--- apps/roundcube/src/js/mailFrameScripts.js
+++ apps/roundcube/src/js/mailFrameScripts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function() {
         $(window).resize(function () {
                 fillWindow($('#roundcube_container'));
@@ -5,58 +6,97 @@ $(document).ready(function() {
         $(window).resize();
         $('#roundcube_container').scroll(updateOnBottom).empty().width($('#content').width());
 });
+=======
+var buffer = 20;
+//scroll bar buffer
+var top_nav_bar_height = 37;
+
+function pageY(elem) {
+	return elem.offsetParent ? (elem.offsetTop + pageY(elem.offsetParent)) : elem.offsetTop;
+}
+
+function resizeIframe() {
+	if (rc.logdebug) {
+		console.log("Starting iFrame resize ...");
+	}
+	var height = document.documentElement.clientHeight;
+	height -= pageY(document.getElementById('roundcubeFrame')) + buffer;
+	height = (height < 0) ? 0 : height;
+	height -= top_nav_bar_height;
+	if (rc.logdebug) {
+		console.log("Resizing height to " + height);
+	}
+	document.getElementById('roundcubeFrame').style.height = height + 'px';
+	// fix scrollbar issue
+	$('#content').css('overflow', 'hidden');
+	$('#content').css('height', '+ height+');
+	width = $('#content').css('width');
+	$('#content').css('width', '+ width +');
+	if (rc.logdebug) {
+		console.log("Resized iFrame to width of " + width + " and height of " + heigth);
+	}
+
+}
+>>>>>>> Fixing layout issues
+
 
 $('#roundcubeFrame').load(function() {
 	var mainscreen = $('#roundcubeFrame').contents().find('#mainscreen');
-    // remove header line, includes about line and
-    var top_line = $('#roundcubeFrame').contents().find('#topline');
-    // correct top padding
-    var top_margin=10;
-    try{    
-            var top_nav = $('#roundcubeFrame').contents().find('#topnav');
-            // check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
-            // TODO refactor and move theme check
-            if(top_nav.height()!=null){
-                    top_margin= 10;
-                    //In larry theme with accounts plugin, we have to move the account selection
-                    var acc_select = top_line.find('.username');
-                    if (acc_select) {
-                          mainscreen.find('div#messagetoolbar').attr('id',''); //Quick n dirty for space
-                          acc_select.appendTo(mainscreen.find('div#searchfilter')).css({position: 'absolute', top:'0px', left:'-170px'});
-                    }
-            } else {
-                    top_margin= parseInt(mainscreen.css('top'),10)-top_line.height();
-            }
-    } catch (e) {}
-    top_line.remove();
-    
-    // fix topbar, issue https://github.com/hypery2k/owncloud/issues/54
-    $('#roundcubeFrame').contents().find('.toolbar').css('z-index','80');
-    $('#roundcubeFrame').contents().find('.toolbar').css('position','absolute');
+	// remove header line, includes about line and
+	var top_line = $('#roundcubeFrame').contents().find('#topline');
+	// correct top padding
+	var top_margin = 10;
+	try {
+		var top_nav = $('#roundcubeFrame').contents().find('#topnav');
+		// check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
+		// TODO refactor and move theme check
+		if (top_nav.height() != null) {
+			top_margin = 10;
+			//In larry theme with accounts plugin, we have to move the account selection
+			var acc_select = top_line.find('.username');
+			if (acc_select) {
+				mainscreen.find('div#messagetoolbar').attr('id', '');
+				//Quick n dirty for space
+				acc_select.appendTo(mainscreen.find('div#searchfilter')).css({
+					position : 'absolute',
+					top : '0px',
+					left : '-170px'
+				});
+			}
+		} else {
+			top_margin = parseInt(mainscreen.css('top'), 10) - top_line.height();
+		}
+	} catch (e) {
+	}
+	top_line.remove();
 
-    // remove logout button
-    $('#roundcubeFrame').contents().find('.button-logout').remove();
-    
-    // check if the header menu from roundcube was disabled
-    if($('#disable_header_nav').val() === 'true') {
+	// fix topbar, issue https://github.com/hypery2k/owncloud/issues/54
+	$('#roundcubeFrame').contents().find('.toolbar').css('z-index', '80');
+	$('#roundcubeFrame').contents().find('.toolbar').css('position', 'absolute');
 
-            var top_nav = $('#roundcubeFrame').contents().find('#header');
-            // check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
-            if(top_nav.height()==null){
-                    top_nav = $('#roundcubeFrame').contents().find('#taskbar');
-            } else {
-                    //top_in= top_margin-top_nav.height();
-            }
-            top_nav.remove();
-		$('#roundcubeFrame').contents().find('#mainscreen').css('top',top_margin);
-    } else {
+	// remove logout button
+	$('#roundcubeFrame').contents().find('.button-logout').remove();
+
+	// check if the header menu from roundcube was disabled
+	if ($('#disable_header_nav').val() === 'true') {
+
+		var top_nav = $('#roundcubeFrame').contents().find('#header');
+		// check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
+		if (top_nav.height() == null) {
+			top_nav = $('#roundcubeFrame').contents().find('#taskbar');
+		} else {
+			//top_in= top_margin-top_nav.height();
+		}
+		top_nav.remove();
+		$('#roundcubeFrame').contents().find('#mainscreen').css('top', top_margin);
+	} else {
 		// correct top padding
-		$('#roundcubeFrame').contents().find('#mainscreen').css('top','50px');
+		$('#roundcubeFrame').contents().find('#mainscreen').css('top', '50px');
 	}
 	// slide in roundcube nice
 	$('#loader').fadeOut('slow');
 	$('#roundcubeFrame').slideDown('slow');
 	// remove email adresse
 	$('#roundcubeFrame').contents().find('.username').remove();
-	
+
 });
--- apps/roundcube/src/js/scripts.js
+++ apps/roundcube/src/js/scripts.js
@@ -1,3 +1,6 @@
+// hlr javascript namespace
+var rc = document.rc || {};
+rc.logdebug = false; // always as false committed, otherwise errors in internet explorer
 // dirty fix for JS errors
 $.fn.extend({showPassword: function(c) {  }});
 $.fn.extend({inFieldLabels: function(d) {  }});
