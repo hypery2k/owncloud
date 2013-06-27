4ebb37b3dd0bd19fc0aaee5c67d2bc3d88a7be45
Martin Reinhardt <Martin.Reinhardt@adesso.de>
2013-06-27 08:44:44 +0200

    fix mysql CI script error

--- bin/ci/prepare_mysql.sh
+++ bin/ci/prepare_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Preparing MySQL environment"
 # mysql setup
-mysql CREATE DATABASE IF NOT EXISTS  roundcubemail_073;
+mysql 'CREATE DATABASE IF NOT EXISTS  roundcubemail_073;'  -u root -p root
 mysql roundcubemail_073 < /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql -u root -p root
-mysql CREATE DATABASE IF NOT EXISTS roundcubemail_082;
+mysql 'CREATE DATABASE IF NOT EXISTS roundcubemail_082;' -u root -p root
 mysql roundcubemail_082 < /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql -u root -p root
