4630dda740c1392de5ab698fa60c3aab38a93e1c
Martin Reinhardt <contact@martinreinhardt-online.de>
2013-07-21 16:29:37 +0200

    Remove password from MySQL seyup

--- bin/ci/prepare_mysql.sh
+++ bin/ci/prepare_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Preparing MySQL environment"
 # mysql setup
-echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_073;'  | mysql -uroot -proot
-echo /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql | mysql -uroot -proot
-echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_082;'  | mysql -uroot -proot
-echo  /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql | mysql -uroot -proot
+echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_073;'  | mysql -uroot
+echo /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql | mysql -uroot
+echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_082;'  | mysql -uroot
+echo  /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql | mysql -uroot
