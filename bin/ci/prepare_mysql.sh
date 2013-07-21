6807f95cb9bd6db030823fff7659469ee55ee7e3
Martin Reinhardt <contact@martinreinhardt-online.de>
2013-07-21 16:39:02 +0200

    More debug info during SQL setup

--- bin/ci/prepare_mysql.sh
+++ bin/ci/prepare_mysql.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 echo "Preparing MySQL environment"
 # mysql setup
+echo "Preparing db for roundcube 0.7.3"
 echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_073;'  | mysql -uroot
-echo /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql | mysql -uroot
+echo "Preparing initial data for roundcube 0.7.3"
+echo /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql
+# echo /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql | mysql -uroot
+echo "Preparing db for roundcube 0.8.2"
 echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_082;'  | mysql -uroot
-echo  /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql | mysql -uroot
+echo "Preparing initial data for roundcube 0.8.2"
+echo /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql
+# echo  /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql | mysql -uroot
