8c4e0fefaaa449f2e0e55a5385fd18784bdf58b2
Martin Reinhardt <contact@martinreinhardt-online.de>
2013-07-21 16:44:08 +0200

    Using correct cmd for showing SQL file conten

--- bin/ci/prepare_mysql.sh
+++ bin/ci/prepare_mysql.sh
@@ -4,10 +4,10 @@ echo "Preparing MySQL environment"
 echo "Preparing db for roundcube 0.7.3"
 echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_073;'  | mysql -uroot
 echo "Preparing initial data for roundcube 0.7.3"
-echo /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql
-# echo /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql | mysql -uroot
+cat /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql
+# cat /var/www/roundcubemail-0.7.3/SQL/mysql.initial.sql | mysql -uroot
 echo "Preparing db for roundcube 0.8.2"
 echo  'CREATE DATABASE IF NOT EXISTS  roundcubemail_082;'  | mysql -uroot
 echo "Preparing initial data for roundcube 0.8.2"
-echo /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql
-# echo  /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql | mysql -uroot
+cat /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql
+# cat  /var/www/roundcubemail-0.8.2/SQL/mysql.initial.sql | mysql -uroot
