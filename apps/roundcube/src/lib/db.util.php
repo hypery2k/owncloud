0d6ede0c3200a0a2f30dc3a13014ad9ff787b697
Martin Reinhardt <contact@martinreinhardt-online.de>
2013-07-24 11:05:31 +0200

    Update db.util.php

--- apps/roundcube/src/lib/db.util.php
+++ apps/roundcube/src/lib/db.util.php
@@ -1,4 +1,24 @@
 <?
+/**
+ * ownCloud - roundcube mail plugin
+ *
+ * @author Martin Reinhardt
+ * @copyright 2013 Martin Reinhardt contact@martinreinhardt-online.de
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 class OC_RoundCube_DB_Util {
 
 /**
@@ -13,7 +33,7 @@ static public function tableExists($table) {
     // Run it in try/catch in case PDO is in ERRMODE_EXCEPTION.
     try {
 
-        $sql = "SELECT 1 FROM $table LIMIT 1";
+        $sql = "SELECT 1 FROM *PREFIX*$table LIMIT 1";
         $args = array(1);
 
         $query = \OCP\DB::prepare($sql);
