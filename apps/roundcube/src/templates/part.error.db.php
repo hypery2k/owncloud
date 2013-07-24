1612f2a82deb487a397793e7c283700a5d66a192
Martin Reinhardt <contact@martinreinhardt-online.de>
2013-07-24 11:10:52 +0200

    Adding db error page

---
+++ apps/roundcube/src/templates/part.error.db.php
@@ -0,0 +1,6 @@
+<div id="dberror">
+    <h1><?php echo $l->t("Database error") ?></h1>
+    <div>
+        <small><?php echo($l->t('The database tables for the roundcube app were not created. Please setup manually.'')); ?></small>
+	</div>
+</div>
