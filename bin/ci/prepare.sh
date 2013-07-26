5269274719e5a7b671fdd3c40ae11d3af036c637
Martin Reinhardt <contact@martinreinhardt-online.de>
2013-07-26 17:06:48 +0200

    Adding current apps sources to CI prepare

--- bin/ci/prepare.sh
+++ bin/ci/prepare.sh
@@ -8,24 +8,30 @@ cd ..
 
 cd ..
 # SETUP OWNCLOUD
+# TODO add stable oc
+# TODO move dir to variables
 echo "Preparing owncloud setup"
 
 sudo mkdir /var/www/mysql
 sudo mkdir /var/www/mysql/4.0.8
 sudo mkdir /var/www/mysql/4.0.8/apps
+sudo mkdir /var/www/mysql/4.0.8/apps/roundcube
 sudo mkdir /var/www/mysql/4.0.8/config
 sudo mkdir /var/www/mysql/4.0.8/data
 sudo mkdir /var/www/mysql/4.5.0
 sudo mkdir /var/www/mysql/4.5.0/apps
+sudo mkdir /var/www/mysql/4.5.0/apps/roundcube 
 sudo mkdir /var/www/mysql/4.5.0/config
 sudo mkdir /var/www/mysql/4.5.0/data
 sudo mkdir /var/www/sqllite
 sudo mkdir /var/www/sqllite/4.0.8
 sudo mkdir /var/www/sqllite/4.0.8/apps
+sudo mkdir /var/www/sqllite/4.0.8/apps/roundcube 
 sudo mkdir /var/www/sqllite/4.0.8/config
 sudo mkdir /var/www/sqllite/4.0.8/data
 sudo mkdir /var/www/sqllite/4.5.0
 sudo mkdir /var/www/sqllite/4.5.0/apps
+sudo mkdir /var/www/sqllite/4.5.0/apps/roundcube 
 sudo mkdir /var/www/sqllite/4.5.0/config
 sudo mkdir /var/www/sqllite/4.5.0/data
 
@@ -35,6 +41,24 @@ sudo cp -rp travis_ci/owncloud_releases/4.5.0 /var/www/mysql/4.5.0
 sudo cp -rp travis_ci/owncloud_releases/4.5.0 /var/www/sqllite/4.5.0
 sudo cp -rp travis_ci/roundcube_releases/0.7.3 /var/www/roundcubemail-0.7.3
 sudo cp -rp travis_ci/roundcube_releases/0.8.2 /var/www/roundcubemail-0.8.2
+
+# prepare roundcube app
+# TODO testdata in db
+echo "copy current app folder"
+sudo mkdir /var/www/mysql/4.0.8/apps
+sudo wget https://github.com/hypery2k/owncloud/tree/master/apps
+sudo ls -lisah /var/www/mysql/4.0.8/apps/*
+sudo mkdir /var/www/mysql/4.5.0/apps
+sudo wget https://github.com/hypery2k/owncloud/tree/master/apps
+sudo ls -lisah /var/www/mysql/4.5.0/apps/*
+sudo mkdir /var/www/sqllite/4.0.8/apps
+sudo wget https://github.com/hypery2k/owncloud/tree/master/apps
+sudo ls -lisah /var/www/sqllite/4.0.8/apps/*
+sudo cd /var/www/sqllite/4.5.0/apps
+sudo wget https://github.com/hypery2k/owncloud/tree/master/apps
+sudo ls -lisah /var/www/sqllite/4.5.0/apps/*
+
+echo "Setting up Directory rights"
 sudo chmod -R 777 /var/www/mysql/4.0.8/apps
 sudo chmod -R 777 /var/www/mysql/4.5.0/apps
 sudo chmod -R 777 /var/www/sqllite/4.0.8/apps
@@ -51,10 +75,13 @@ sudo chmod -R 777 /var/www/roundcubemail-0.7.3
 sudo chmod -R 777 /var/www/roundcubemail-0.8.2
 
 echo "Done with general owncloud setup"
+
+echo " preparing OwnCloud db"
 # admin Passw0rd!
 sudo cp  travis_ci/sqllite/4.0.8/config.php /var/www/sqllite/4.0.8/config/
 sudo cp  travis_ci/sqllite/4.0.8/owncloud.db /var/www/sqllite/4.0.8/data/
 
+echo "Setting up IMAP test environment"
 # setup dovecot
 #Create temporary file with new line in place
 sudo sed -e 's/mail_location.*=.*/mail_location = mbox:~\/mail:INBOX=\/var\/spool\/mail\/%u # (for mbox)/g' /etc/dovecot/dovecot.conf > /tmp/dovecot.conf
@@ -66,4 +93,6 @@ sudo service dovecot start
 # setup IMAP testuser, with password imap
 sudo useradd imap1 -g users -d /home/imap1 -s /bin/bash -p imap -m
 sudo useradd imap2 -g users -d /home/imap2 -s /bin/bash -p imap -m
+
+echo "Done with setup of test environment"
 
