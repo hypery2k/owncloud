96ac95bf5158bc00a6b94ba8b1fe8e282e3ed37a
Martin Reinhardt <M.Reinhardt@profi-ag.de>
2013-07-29 17:39:00 +0200

    Fix error messages
    see #134

--- apps/roundcube/src/templates/part.error.db.php
+++ apps/roundcube/src/templates/part.error.db.php
@@ -1,6 +1,7 @@
 <div id="dberror">
     <h1><?php echo $l->t("Database error") ?></h1>
     <div>
-        <small><?php echo($l->t('The database tables for the roundcube app were not created. Please setup manually.'')); ?></small>
+        <small><?php echo $l -> t('Please follow the wiki page to setup the database manually:') ?></small>
+	    <a class="button" target="_blank" href="https://github.com/hypery2k/owncloud/wiki/Installation#manual-database-configuration">Wiki</a>
 	</div>
 </div>
--- apps/roundcube/src/templates/part.error.no-settings.php
+++ apps/roundcube/src/templates/part.error.no-settings.php
@@ -1,7 +1,7 @@
 <div id="firstrun">
     <h1><?php echo $l->t("You don't have any email account configured yet.") ?></h1>
     <div>
-        <small><?php echo($l->t('You can manage your email accounts here:')); ?></small>
-	    <a class="button" href ="/index.php/settings/users"><?php echo $l->t('Settings'); ?></a>
+        <small><?php echo $l->t('You can manage your email accounts here:') ?></small>
+	    <a class="button" href ="/index.php/settings/users"><?php echo $l -> t('Settings'); ?></a>
 	</div>
 </div>
