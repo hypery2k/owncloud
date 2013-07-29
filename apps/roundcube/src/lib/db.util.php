16a7c2e64d32e556ccd0a78dce1da520da510204
Martin Reinhardt <Martin.Reinhardt@adesso.de>
2013-07-29 06:53:28 +0200

    More debug information on db util classs

--- apps/roundcube/src/lib/db.util.php
+++ apps/roundcube/src/lib/db.util.php
@@ -21,32 +21,35 @@
  */
 class OC_RoundCube_DB_Util {
 
-/**
- * Check if a table exists in the current database.
- *
- * @param string $table Table to search for.
- * @return bool TRUE if table exists, FALSE if no table found.
- */
-static public function table_exists($table) {
+	/**
+	 * Check if a table exists in the current database.
+	 *
+	 * @param string $table Table to search for.
+	 * @return bool TRUE if table exists, FALSE if no table found.
+	 */
+	static public function table_exists($table) {
+		OCP\Util::writeLog('roundcube', 'OC_RoundCube_DB_Util.class.php: ' . 'Checking if table ' . $table . ' exists.', OCP\Util::DEBUG);
+		// Try a select statement against the table
+		// Run it in try/catch in case PDO is in ERRMODE_EXCEPTION.
+		try {
 
-    // Try a select statement against the table
-    // Run it in try/catch in case PDO is in ERRMODE_EXCEPTION.
-    try {
+			$sql = "SELECT 1 FROM *PREFIX*$table LIMIT 1";
+			$args = array(1);
 
-        $sql = "SELECT 1 FROM *PREFIX*$table LIMIT 1";
-        $args = array(1);
+			$query = \OCP\DB::prepare($sql);
+			$result = $query -> execute($args);
 
-        $query = \OCP\DB::prepare($sql);
-        $result = $query->execute($args);
+		} catch (Exception $e) {
+			// We got an exception == table not found
+			OCP\Util::writeLog('roundcube', 'OC_RoundCube_DB_Util.class.php: ' . 'Table ' . $table . ' does not exists.', OCP\Util::DEBUG);
+			return FALSE;
+		}
 
-    } catch (Exception $e) {
-        // We got an exception == table not found
-        return FALSE;
-    }
+		// Result is either boolean FALSE (no table found) or PDOStatement Object (table found)
+		OCP\Util::writeLog('roundcube', 'OC_RoundCube_DB_Util.class.php: ' . 'Result of check for if table ' . $table . ' exists: ' . $result, OCP\Util::DEBUG);
+		return $result !== FALSE;
 
-    // Result is either boolean FALSE (no table found) or PDOStatement Object (table found)
-    return $result !== FALSE;
+	}
 
-}
 }
 ?>
