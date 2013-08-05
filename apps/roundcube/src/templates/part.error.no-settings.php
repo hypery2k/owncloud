1cee8e73130b8f7d315279270081b61a8e9aeabb
olafc <fake@olafchrist.de>
2013-08-06 00:05:12 +0200

    Fix personal settings link
    This is not the optimal solution, as link_to() should probably only used
    to link inside of apps ( wget
    http://192.168.1.100/part.error.no-settings.php). Nevertheless it works
    for me...

--- apps/roundcube/src/templates/part.error.no-settings.php
+++ apps/roundcube/src/templates/part.error.no-settings.php
@@ -2,6 +2,6 @@
     <h1><?php echo $l->t("You don't have any email account configured yet.") ?></h1>
     <div>
         <small><?php echo $l->t('You can manage your email accounts here:') ?></small>
-	    <a class="button" href="/<?php OCP\Util::getRequestUri()?>/index.php/settings/users"><?php echo $l -> t('Settings'); ?></a>
+	    <a class="button" href="<?php print_unescaped(link_to('index.php', 'settings/personal')) ?>"><?php echo $l -> t('Settings'); ?></a>
 	</div>
 </div>
