b764e904bd68d31efdbe7ff42d6dd0d14832b432
Martin Reinhardt <Martin.Reinhardt@adesso.de>
2013-09-02 20:09:59 +0200

    Improve JS handling
    see #130

--- apps/roundcube/src/js/mailFrameScripts.js
+++ apps/roundcube/src/js/mailFrameScripts.js
@@ -13,64 +13,86 @@ $(document).ready(function() {
 	//$('#roundcube_container').scroll(updateOnBottom).empty().width($('#content').width());
 });
 
+/**
+ * init js function during frame load
+ */
 $('#roundcubeFrame').load(function() {
 	var mainscreen = $('#roundcubeFrame').contents().find('#mainscreen');
 	// remove header line, includes about line and
 	var top_line = $('#roundcubeFrame').contents().find('#topline');
 	// correct top padding
 	var top_margin = 10;
-	
-	var rc_version ='';
+
+	var rc_version = '';
+	var rc_theme = '';
+
 	try {
 		var top_nav = $('#roundcubeFrame').contents().find('#topnav');
 		// check if the above element exits (only in new larry theme, if null use rc 0.7 default theme
-		// TODO refactor and move theme check
+
 		if (top_nav.height() != null) {
-			top_margin = 10;
-			//In larry theme with accounts plugin, we have to move the account selection
-			var acc_select = top_line.find('.username');
-			if (acc_select) {
-				mainscreen.find('div#messagetoolbar').attr('id', 'ocrcMessagetoolbar');
-				var searchfilter = mainscreen.find('div#searchfilter');
-				//Quick n dirty for space
-				acc_select.appendTo(searchfilter);
-				searchfilter.css({
-					position : 'absolute',
-					top : '0px',
-					width : '160px',
-					right : '256px',
-					top : '7px'
-				});
-				searchfilter.find('select#rcmlistfilter').css('width', '158px');
-				searchfilter.find('a.menuselector').css('width', '158px');
-				searchfilter.find('span.handle').css('width', '120px');
-				var messagetoolbar = mainscreen.find('div#ocrcMessagetoolbar');
-				messagetoolbar.css({
-					left : '0',
-					right : '390px'
-				});
-				//Extend messagetoolbar, if fullwidth is specified
-				mainscreen.find('.fullwidth').css('right', '0px');
-				var toolbarselect = messagetoolbar.find('.toolbarselect');
-				toolbarselect.css('position', 'absolute');
-				toolbarselect.css('bottom', '6px');
-				toolbarselect.css('right', '3px');
+			rc_theme = 'larry';
+
+			var minimize_toggle = $('#roundcubeFrame').contents().find('.minmodetoggle');
+			if (minimize_toggle.height() != null) {
+				rc_version = "0-9";
+			} else {
+				rc_version = "0-8";
 			}
 		} else {
-			rc_version="0-7";
-			top_margin = parseInt(mainscreen.css('top'), 10) - top_line.height();
-			// fix layout button issue on roundcube 0.7
-			$('#roundcubeFrame').contents().find('#messagetoolbar').css('padding','20px 6px 5px 0px');
-			$('#roundcubeFrame').contents().find('#messagetoolbar').css('z-index','100');
+			rc_version = "0-7";
 		}
-		var minimize_toggle = $('#roundcubeFrame').contents().find('.minmodetoggle');
-		if (minimize_toggle.height() != null){
-			rc_version="0-9";
-			$('#roundcubeFrame').contents().find('body').removeAttr('class');
-			$('#roundcubeFrame').contents().find('.minmodetoggle').remove();
+
+		switch (rc_theme) {
+			case "larry":
+				top_margin = 10;
+				//In larry theme with accounts plugin, we have to move the account selection
+				var acc_select = top_line.find('.username');
+				if (acc_select) {
+					mainscreen.find('div#messagetoolbar').attr('id', 'ocrcMessagetoolbar');
+					var searchfilter = mainscreen.find('div#searchfilter');
+					//Quick n dirty for space
+					acc_select.appendTo(searchfilter);
+					searchfilter.css({
+						position : 'absolute',
+						top : '0px',
+						width : '160px',
+						right : '256px',
+						top : '7px'
+					});
+					searchfilter.find('select#rcmlistfilter').css('width', '158px');
+					searchfilter.find('a.menuselector').css('width', '158px');
+					searchfilter.find('span.handle').css('width', '120px');
+					var messagetoolbar = mainscreen.find('div#ocrcMessagetoolbar');
+					messagetoolbar.css({
+						left : '0',
+						right : '390px'
+					});
+					//Extend messagetoolbar, if fullwidth is specified
+					mainscreen.find('.fullwidth').css('right', '0px');
+					var toolbarselect = messagetoolbar.find('.toolbarselect');
+					toolbarselect.css('position', 'absolute');
+					toolbarselect.css('bottom', '6px');
+					toolbarselect.css('right', '3px');
+				}
+				break;
 		}
-		
-	} catch (e) {
+
+		switch (rc_version) {
+			case "0-7":
+				rc_version = "0-7";
+				top_margin = parseInt(mainscreen.css('top'), 10) - top_line.height();
+				// fix layout button issue on roundcube 0.7
+				$('#roundcubeFrame').contents().find('#messagetoolbar').css('padding', '20px 6px 5px 0px');
+				$('#roundcubeFrame').contents().find('#messagetoolbar').css('z-index', '100');
+				break;
+			case "0-9":
+				$('#roundcubeFrame').contents().find('body').removeAttr('class');
+				$('#roundcubeFrame').contents().find('.minmodetoggle').remove();
+				break;
+
+		}
+	} catch(e) {
 	}
 	top_line.remove();
 
