216b12448b3dc2d15d5565ec5361380610614171
Martin Reinhardt <M.Reinhardt@profi-ag.de>
2013-09-30 08:20:49 +0200

    Adding try/catch to ommit errors during invoking hooks
    see #168

--- apps/roundcube/src/lib/RoundCubeAuthHelper.class.php
+++ apps/roundcube/src/lib/RoundCubeAuthHelper.class.php
@@ -22,22 +22,34 @@
 class OC_RoundCube_AuthHelper {
 
 	public static function autoSave($params) {
-		OCP\Util::writeLog('roundcube', 'Preparing autologin of user to roundcube', OCP\Util::INFO);
-		$mail_userdata_entries = OC_RoundCube_App::checkLoginData(OCP\User::getUser());
-		// TODO add multi-user support
-		$myID = $mail_userdata_entries[0];
-		$mail_user = OC_RoundCube_App::cryptMyEntry($params['uid']);
-		$mail_password = OC_RoundCube_App::cryptMyEntry($params['password']);
-		$stmt = OCP\DB::prepare("UPDATE *PREFIX*roundcube SET mail_user = ?, mail_password = ? WHERE id = ?");
-		$result = $stmt -> execute(array($mail_user, $mail_password, $myID));
-		OCP\Util::writeLog('roundcube', 'Autologin of user to roundcube done', OCP\Util::INFO);
+		try {
+			OCP\Util::writeLog('roundcube', 'Preparing autologin of user to roundcube', OCP\Util::INFO);
+			$mail_userdata_entries = OC_RoundCube_App::checkLoginData(OCP\User::getUser());
+			// TODO add multi-user support
+			$myID = $mail_userdata_entries[0];
+			$mail_user = OC_RoundCube_App::cryptMyEntry($params['uid']);
+			$mail_password = OC_RoundCube_App::cryptMyEntry($params['password']);
+			$stmt = OCP\DB::prepare("UPDATE *PREFIX*roundcube SET mail_user = ?, mail_password = ? WHERE id = ?");
+			$result = $stmt -> execute(array($mail_user, $mail_password, $myID));
+			OCP\Util::writeLog('roundcube', 'Autologin of user to roundcube done', OCP\Util::INFO);
+		} catch (Exception $e) {
+			// We got an exception == table not found
+			OCP\Util::writeLog('roundcube', 'OC_RoundCube_AuthHelper.class.php: ' . 'Autologin causing errors.'.$e, OCP\Util::DEBUG);
+			return false;
+		}
 	}
 
 	public static function logout($params) {
-		OCP\Util::writeLog('roundcube', 'Preparing logout of user from roundcube', OCP\Util::INFO);
-		$maildir = OCP\Config::getAppValue('roundcube', 'maildir', '');
-		OC_RoundCube_App::logout($maildir, OCP\User::getUser());
-		OCP\Util::writeLog('roundcube', 'Logout of user from roundcube done', OCP\Util::INFO);
+		try {
+			OCP\Util::writeLog('roundcube', 'Preparing logout of user from roundcube', OCP\Util::INFO);
+			$maildir = OCP\Config::getAppValue('roundcube', 'maildir', '');
+			OC_RoundCube_App::logout($maildir, OCP\User::getUser());
+			OCP\Util::writeLog('roundcube', 'Logout of user from roundcube done', OCP\Util::INFO);
+		} catch (Exception $e) {
+			// We got an exception == table not found
+			OCP\Util::writeLog('roundcube', 'OC_RoundCube_AuthHelper.class.php: ' .  'Logout/Session cleaning causing errors.'.$, OCP\Util::DEBUG);
+			return false;
+		}
 	}
 
 }
