b8409094c0f0d28113b07a33ac4c90fa31bc6be6
Claus-Justus Heine <himself@claus-justus-heine.de>
2013-11-27 10:03:16 +0100

    Because the initial visibility of the preview-pane is display:none (as it is part of the iframe which initially is hidden) RC thinks that the preview frame is not there. Convince RC "by-force" that we have a preview-pane (if that is present on the page).

--- apps/roundcube/js/mail.js
+++ apps/roundcube/js/mail.js
@@ -152,4 +152,20 @@ rc.iframe_loaded = function() {
 	// remove email adresse
 	$('#roundcubeFrame').contents().find('.username').remove();
 
+        // RC only checks for visibility of the preview
+        // window. However, as with start with display:none for the
+        // entire frame, the prevew window is indeed not visible at
+        // the start when RC initializes itself.
+        var mailPrev = $('#roundcubeFrame').contents().find('#mailpreviewframe');
+        if (mailPrev.length && mailPrev.css('display') != 'none') {
+                $('#roundcubeFrame')[0].contentWindow.rcmail.set_env('contentframe','messagecontframe');
+        }
+
 };
+
+/*
+ * Local Variables: ***
+ * js-indent-level: 8 ***
+ * End: ***
+ */
+
