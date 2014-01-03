0e47a432f27bf576ffd5bc1dbc7df0c9a50ec592
Martin Reinhardt <M.Reinhardt@profi-ag.de>
2014-01-03 15:58:15 +0100

    JS code cleanup
    see 189

--- apps/roundcube/js/userSettings.js
+++ apps/roundcube/js/userSettings.js
@@ -1,46 +1,47 @@
 $(document).ready(function() {
+	try {
+		// 'show password' checkbox
+		var element = $('#mail_password');
+		var tmp = element.val();
+		var placeholder = element.attr('placeholder');
+		element.showPassword();
+		element.val(tmp);
 
-  // 'show password' checkbox
-  var element     = $('#usermail #mail_password');
-  var tmp         = element.val();
-  var placeholder = element.attr('placeholder');
-  element.showPassword();
-  element.val(tmp);
+		// Pseudo subit. In General, we do not like submitting forms in
+		// favor of the Ajax technology. Its just more handy and less
+		// error-prone.
 
-  // Pseudo subit. In General, we do not like submitting forms in
-  // favor of the Ajax technology. Its just more handy and less
-  // error-prone.
+		$("#usermail_update").click(function() {
+			event.preventDefault();
 
-  $("#usermail_update").click(function() {
-    event.preventDefault();
+			var self = $(this);
+			var password = $('#usermail #mail_password').val();
+			var user = $('#usermail #mail_username').val();
 
-    var self = $(this);
-    var password = $('#usermail #mail_password').val();
-    var user     = $('#usermail #mail_username').val();
+			$('div.statusmessage').hide();
+			$('span.statusmessage').hide();
+			if (password != '' && user != '') {
+				// Serialize the data
+				var post = $("#usermail").serialize();
+				// Ajax foo
+				$.post(OC.filePath('roundcube', 'ajax', 'userSettings.php'), post, function(data) {
+					if (data.status == 'success') {
+						$('#usermail_update_message').html(data.data.message);
+						$('#usermail_update_message').show();
+					} else {
+						$('#usermail_update_message').html(data.data.message);
+						$('#usermail_update_message').show();
+					}
+				}, 'json');
+				return false;
+			} else {
+				$('#userkey #error').show();
+				return false;
+			}
 
-    $('div.statusmessage').hide();
-    $('span.statusmessage').hide();
-    if (password != '' && user != '') {
-      // Serialize the data
-      var post = $("#usermail").serialize();
-      // Ajax foo
-      $.post(OC.filePath('roundcube', 'ajax', 'userSettings.php'),
-             post, function(data) {
-               if (data.status == 'success') {
-                 $('#usermail_update_message').html(data.data.message);
-                 $('#usermail_update_message').show();
-               } else {
-                 $('#usermail_update_message').html(data.data.message);
-                 $('#usermail_update_message').show();
-               }
-             },'json');
-      return false;
-    } else {
-      $('#userkey #error').show();
-      return false;
-    }
-    
-  });
+		});
+	} catch(e) {
+	}
 
 });
 
