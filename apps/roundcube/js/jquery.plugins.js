f1be5819acc74bc443daeee3683d26a8bc6503b2
Martin Reinhardt <M.Reinhardt@profi-ag.de>
2014-01-03 11:54:47 +0100

    Fix oc menu issue
    see #16

--- apps/roundcube/js/jquery.plugins.js
+++ apps/roundcube/js/jquery.plugins.js
@@ -1,7 +1,4 @@
-// roundcube javascript namespace
-var rc = document.rc || {};
-rc.logdebug = false; // always as false committed, otherwise errors in internet explorer
-// dirty fix for JS errors
-$.fn.extend({showPassword: function(c) {  }});
-$.fn.extend({inFieldLabels: function(d) {  }});
-$.fn.extend({tipsy: function(e) {  }});
+// overcome Owncloud JS errors
+-$.fn.extend({showPassword: function(c) { return $('#searchbox'); }});
+-$.fn.extend({inFieldLabels: function(d) {  }});
+-$.fn.extend({tipsy: function(e) {  }});
