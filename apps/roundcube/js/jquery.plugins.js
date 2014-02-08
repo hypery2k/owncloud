3d8072ca266ec00c4de9065e4407f45633cde508
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-02-08 19:36:19 +0100

    Fix JS error
    fixes #204

--- apps/roundcube/js/jquery.plugins.js
+++ apps/roundcube/js/jquery.plugins.js
@@ -2,6 +2,19 @@
 var rc = document.rc || {};
 rc.logdebug = false; // always as false committed, otherwise errors in internet explorer
 // dirty fix for JS errors
-$.fn.extend({showPassword: function(c) {  }});
-$.fn.extend({inFieldLabels: function(d) {  }});
-$.fn.extend({tipsy: function(e) {  }});
+$.fn.extend({
+    showPassword: function (c) {
+
+        return this.each(function () {
+            $checkbox = $($input.data('typetoggle'));
+
+            $input.bind('keyup', function () {
+                update($input, $clone)
+            });
+        });
+    }
+});
+$.fn.extend({inFieldLabels: function (d) {
+}});
+$.fn.extend({tipsy: function (e) {
+}});
