f7ebb91aaf561c1b88417522041fe70f1e4e7578
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-05-30 00:38:44 +0200

    Adding roundcube 0.9 setup

--- environment/mysql/OC60/create_db.sql
+++ environment/mysql/OC60/create_db.sql
@@ -1,19 +1,259 @@
--- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: oc_testing
--- ------------------------------------------------------
--- Server version	5.5.37-0ubuntu0.12.04.1-log
+-- ROUNDCUBE ---
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+--
+-- Table structure for table `cache`
+--
+
+DROP TABLE IF EXISTS `cache`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cache` (
+  `user_id` int(10) unsigned NOT NULL,
+  `cache_key` varchar(128) CHARACTER SET ascii NOT NULL,
+  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `data` longtext NOT NULL,
+  KEY `created_index` (`created`),
+  KEY `user_cache_index` (`user_id`,`cache_key`),
+  CONSTRAINT `user_id_fk_cache` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cache`
+--
+
+LOCK TABLES `cache` WRITE;
+/*!40000 ALTER TABLE `cache` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cache` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cache_index`
+--
+
+DROP TABLE IF EXISTS `cache_index`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cache_index` (
+  `user_id` int(10) unsigned NOT NULL,
+  `mailbox` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `valid` tinyint(1) NOT NULL DEFAULT '0',
+  `data` longtext NOT NULL,
+  PRIMARY KEY (`user_id`,`mailbox`),
+  KEY `changed_index` (`changed`),
+  CONSTRAINT `user_id_fk_cache_index` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cache_index`
+--
+
+LOCK TABLES `cache_index` WRITE;
+/*!40000 ALTER TABLE `cache_index` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cache_index` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cache_messages`
+--
+
+DROP TABLE IF EXISTS `cache_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cache_messages` (
+  `user_id` int(10) unsigned NOT NULL,
+  `mailbox` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `uid` int(11) unsigned NOT NULL DEFAULT '0',
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `data` longtext NOT NULL,
+  `flags` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`mailbox`,`uid`),
+  KEY `changed_index` (`changed`),
+  CONSTRAINT `user_id_fk_cache_messages` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cache_messages`
+--
+
+LOCK TABLES `cache_messages` WRITE;
+/*!40000 ALTER TABLE `cache_messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cache_messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cache_thread`
+--
+
+DROP TABLE IF EXISTS `cache_thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cache_thread` (
+  `user_id` int(10) unsigned NOT NULL,
+  `mailbox` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `data` longtext NOT NULL,
+  PRIMARY KEY (`user_id`,`mailbox`),
+  KEY `changed_index` (`changed`),
+  CONSTRAINT `user_id_fk_cache_thread` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cache_thread`
+--
+
+LOCK TABLES `cache_thread` WRITE;
+/*!40000 ALTER TABLE `cache_thread` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cache_thread` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contactgroupmembers`
+--
+
+DROP TABLE IF EXISTS `contactgroupmembers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contactgroupmembers` (
+  `contactgroup_id` int(10) unsigned NOT NULL,
+  `contact_id` int(10) unsigned NOT NULL,
+  `created` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  PRIMARY KEY (`contactgroup_id`,`contact_id`),
+  KEY `contactgroupmembers_contact_index` (`contact_id`),
+  CONSTRAINT `contactgroup_id_fk_contactgroups` FOREIGN KEY (`contactgroup_id`) REFERENCES `contactgroups` (`contactgroup_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `contact_id_fk_contacts` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactgroupmembers`
+--
+
+LOCK TABLES `contactgroupmembers` WRITE;
+/*!40000 ALTER TABLE `contactgroupmembers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contactgroupmembers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contactgroups`
+--
+
+DROP TABLE IF EXISTS `contactgroups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contactgroups` (
+  `contactgroup_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `del` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  PRIMARY KEY (`contactgroup_id`),
+  KEY `contactgroups_user_index` (`user_id`,`del`),
+  CONSTRAINT `user_id_fk_contactgroups` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactgroups`
+--
+
+LOCK TABLES `contactgroups` WRITE;
+/*!40000 ALTER TABLE `contactgroups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contactgroups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contacts` (
+  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `del` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `email` text NOT NULL,
+  `firstname` varchar(128) NOT NULL DEFAULT '',
+  `surname` varchar(128) NOT NULL DEFAULT '',
+  `vcard` longtext,
+  `words` text,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`contact_id`),
+  KEY `user_contacts_index` (`user_id`,`del`),
+  CONSTRAINT `user_id_fk_contacts` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacts`
+--
+
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dictionary`
+--
+
+DROP TABLE IF EXISTS `dictionary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dictionary` (
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `language` varchar(5) NOT NULL,
+  `data` longtext NOT NULL,
+  UNIQUE KEY `uniqueness` (`user_id`,`language`),
+  CONSTRAINT `user_id_fk_dictionary` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dictionary`
+--
+
+LOCK TABLES `dictionary` WRITE;
+/*!40000 ALTER TABLE `dictionary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dictionary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `identities`
+--
+
+DROP TABLE IF EXISTS `identities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `identities` (
+  `identity_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `del` tinyint(1) NOT NULL DEFAULT '0',
+  `standard` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(128) NOT NULL,
+  `organization` varchar(128) NOT NULL DEFAULT '',
+  `email` varchar(128) NOT NULL,
+  `reply-to` varchar(128) NOT NULL DEFAULT '',
+  `bcc` varchar(128) NOT NULL DEFAULT '',
+  `signature` text,
+  `html_signature` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`identity_id`),
+  KEY `user_identities_index` (`user_id`,`del`),
+  KEY `email_identities_index` (`email`,`del`),
+  CONSTRAINT `user_id_fk_identities` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+-- OWNCLOUD ---
 
 --
 -- Table structure for table `oc6_activity`
