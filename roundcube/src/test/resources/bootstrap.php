4e8937c5a8c178635ef0713fd985b9c573a13341
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-06-04 19:50:29 +0200

    Fix maven test errors

--- roundcube/src/test/resources/bootstrap.php
+++ roundcube/src/test/resources/bootstrap.php
@@ -3,7 +3,7 @@
 
 define('PHPUNIT_RUN', 1);
 
-require_once __DIR__ . '/../appinfo/app.php';
+//require_once __DIR__ . '/../appinfo/app.php';
 
 if (!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
