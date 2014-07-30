1420897f8b9082f5a855f22dd60a88514f47b3a1
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-07-30 15:58:17 +0200

    Removed one inline-script relict.
    In the iframe tag there was a spurious "oncom=" string in front of the
    name attribute. Clearly, that had been forgotten while removing js
    inline scripting.

--- roundcube/src/main/php/lib/RoundCubeApp.class.php
+++ roundcube/src/main/php/lib/RoundCubeApp.class.php
@@ -336,7 +336,7 @@ class OC_RoundCube_App {
 
 			// create iFrame begin
 			$returnObject -> appendHtmlOutput('<img src="' . $loader_image . '" id="loader">');
-			$returnObject -> appendHtmlOutput('<iframe src="' . $rcl -> getRedirectPath() . '" id="roundcubeFrame"  oncom=name="roundcube" width="100%" width="100%" style="display:none;">  </iframe>');
+			$returnObject -> appendHtmlOutput('<iframe src="' . $rcl -> getRedirectPath() . '" id="roundcubeFrame"  name="roundcube" width="100%" width="100%" style="display:none;">  </iframe>');
 			$returnObject -> appendHtmlOutput('<input type="hidden" id="disable_header_nav" value="' . $disable_header_nav . '"/>');
 			$returnObject -> appendHtmlOutput('<input type="hidden" id="disable_control_nav" value="' . $disable_control_nav . '"/>');
 			// create iFrame end
