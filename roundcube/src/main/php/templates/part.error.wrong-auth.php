d1d8912aec18f80d330a499e8118d2396257e1a4
Claus-Justus Heine <himself@claus-justus-heine.de>
2014-07-30 18:40:09 +0200

    The anchor was missing the HREF to the config page.

--- roundcube/src/main/php/templates/part.error.wrong-auth.php
+++ roundcube/src/main/php/templates/part.error.wrong-auth.php
@@ -2,6 +2,6 @@
     <h1><?php echo $l->t("You don't have any email account configured correctly yet. Please check you username and password.") ?></h1>
     <div>
        <small><?php echo $l->t('You can manage your email accounts here:') ?></small>
-	    <a class="button"><?php echo $l -> t('Settings'); ?></a>
+	    <a class="button" href="<?php print_unescaped(OCP\Util::linkToRoute('settings_personal')) ?>"><?php echo $l -> t('Settings'); ?></a>
 	</div>
 </div>
