7c85ce5fcde6ef6b5a20d02f00daebb4d9f610d8
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-07-30 21:55:54 +0200

    Move l10n script to root folder

--- bin/update.sh
+++ bin/update.sh
@@ -5,9 +5,14 @@ if test $# -ne 1; then
 	exit 1;
 fi
 
+CUR_DIR="$(pwd)"
+
 APPNAME="$1"
 
-perl ./l10n.pl "${APPNAME}" read
+L10N_DIR="../"${APPNAME}"/src/main/php/l10n"
+
+cd ${L10N_DIR}
+perl ${CUR_DIR}"/l10n.pl" "${APPNAME}" read
 msgmerge -vU --previous --backup=numbered de/"${APPNAME}".po  templates/"${APPNAME}".pot
-perl ./l10n.pl "${APPNAME}" write
+perl ${CUR_DIR}"/l10n.pl" "${APPNAME}" write
 
