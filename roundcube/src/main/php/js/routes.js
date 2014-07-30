7693113ff9e7f99905a7cacc6093f9b8856e082b
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-07-30 11:26:20 +0200

    Fix OC7 errors
    see #241

--- roundcube/src/main/php/js/routes.js
+++ roundcube/src/main/php/js/routes.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-    OC.Router.registerLoadedCallback(function(){
-        var url = OC.Router.generate('roundcube_refresh');
-        setInterval(function(){
-            $.post(url);
-        }, 30000);
-    });
+$(document).ready(function() {
+	OC.Router.registerLoadedCallback(function() {
+		var url;
+		if (OC.Router) {
+			url = OC.Router.generate('roundcube_refresh');
+		} else {
+			url = OC.generateUrl('roundcube_refresh');
+		}
+		setInterval(function() {
+			$.post(url);
+		}, 30000);
+	});
 });
