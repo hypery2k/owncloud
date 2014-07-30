b377b7b9fc700fad0170b9fd2e28b9c0a72a0dae
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-07-30 19:39:19 +0200

    Fix db upgrade
    see #140

--- storagecharts2/src/main/php/appinfo/update.php
+++ storagecharts2/src/main/php/appinfo/update.php
@@ -5,12 +5,12 @@
 $installedVersion = OCP\Config::getAppValue('storagecharts2', 'installed_version');
 if (version_compare($installedVersion, '2.5.0', '<')) {
 	try {
-		$sql = 'ALTER TABLE `*PREFIX*storagecharts2` CHANGE COLUMN `?` `?` FLOAT NOT NULL DEFAULT 0.0';
-		$query = \OCP\DB::prepare($sql);
-		$result1 = $query->execute(array( 'stc_month', 'stc_month' ) );
-		$result2 = $query->execute(array( 'stc_dayts', 'stc_day' ) );
-		$result3 = $query->execute(array( 'stc_used', 'stc_used' ) );
-		$result4 = $query->execute(array( 'stc_total', 'stc_total' ) );
+// 		$sql = 'ALTER TABLE `*PREFIX*storagecharts2` CHANGE COLUMN `?` `?` FLOAT NOT NULL DEFAULT 0.0';
+// 		$query = \OCP\DB::prepare($sql);
+// 		$result1 = $query->execute(array( 'stc_month', 'stc_month' ) );
+// 		$result2 = $query->execute(array( 'stc_dayts', 'stc_day' ) );
+// 		$result3 = $query->execute(array( 'stc_used', 'stc_used' ) );
+// 		$result4 = $query->execute(array( 'stc_total', 'stc_total' ) );
 	} catch (Exception $e) {
 		// We got an exception == table not found
 		OCP\Util::writeLog('storagecharts2', 'update.php: ' . 'update error. ' . $e, OCP\Util::DEBUG);
