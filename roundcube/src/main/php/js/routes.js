2d65e752fd50bd256b7bb9d55ddb7a4e06e39673
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-08-16 13:47:38 +0200

    Adding refresh interval set check
    see #252

--- roundcube/src/main/php/js/routes.js
+++ roundcube/src/main/php/js/routes.js
@@ -3,19 +3,21 @@
 var Roundcube = Roundcube || {};
 
 Roundcube.routes = function() {
-  if (OC.Router) {
-    OC.Router.registerLoadedCallback(function() {
-      var url = OC.Router.generate('roundcube_refresh');
+  if (Roundcube.refreshInterval) {
+    if (OC.Router) {
+      OC.Router.registerLoadedCallback(function() {
+	var url = OC.Router.generate('roundcube_refresh');
+	setInterval(function() {
+	  $.post(url);
+	}, Roundcube.refreshInterval * 1000);
+      });
+      // starting with OC7 OC.Router was removed
+    } else {
+      var url = OC.generateUrl('apps/roundcube/' + 'refresh');
       setInterval(function() {
 	$.post(url);
       }, Roundcube.refreshInterval * 1000);
-    });
-    // starting with OC7 OC.Router was removed
-  } else {
-    var url = OC.generateUrl('apps/roundcube/' + 'refresh');
-    setInterval(function() {
-      $.post(url);
-    }, Roundcube.refreshInterval * 1000);
+    }
   }
 }
 
