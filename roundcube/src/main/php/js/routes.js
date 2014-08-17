a543f86dc1219162002855fbda2f0e79c1c0f310
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-08-17 13:19:13 +0200

    add check for logged out
    fixes #256

--- roundcube/src/main/php/js/routes.js
+++ roundcube/src/main/php/js/routes.js
@@ -2,22 +2,38 @@
 
 var Roundcube = Roundcube || {};
 
+/**
+ * Set client side refresh job
+ */
 Roundcube.routes = function() {
-  if (Roundcube.refreshInterval) {
-    if (OC.Router) {
-      OC.Router.registerLoadedCallback(function() {
-	var url = OC.Router.generate('roundcube_refresh');
-	setInterval(function() {
-	  $.post(url);
+  if (OC.currentUser) {
+    if (Roundcube.refreshInterval) {
+      if (OC.Router) {
+	OC.Router.registerLoadedCallback(function() {
+	  var url = OC.Router.generate('roundcube_refresh');
+	  Roundcube.refresh = setInterval(function() {
+	    $.post(url);
+	  }, Roundcube.refreshInterval * 1000);
+	  return true;
+	});
+	// starting with OC7 OC.Router was removed
+      } else {
+	var url = OC.generateUrl('apps/roundcube/' + 'refresh');
+	Roundcube.refresh = setInterval(function() {
+	  if (OC.currentUser) {
+	    $.post(url);
+	  } else {
+	    // if user is null end up refresh (logged out)
+	    clearTimeout(Roundcube.refresh);
+	  }
 	}, Roundcube.refreshInterval * 1000);
-      });
-      // starting with OC7 OC.Router was removed
-    } else {
-      var url = OC.generateUrl('apps/roundcube/' + 'refresh');
-      setInterval(function() {
-	$.post(url);
-      }, Roundcube.refreshInterval * 1000);
+	return true;
+      }
     }
+  } else {
+    // if user is null end up refresh (logged out)
+    clearTimeout(Roundcube.refresh);
+    return false;
   }
 }
 
