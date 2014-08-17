151f9e2c44aa3e481b549f55309bfd9af3174a1e
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-08-17 13:18:28 +0200

    adding Js unit test
    see #252

--- roundcube/src/test/webapp/frontend/routes.js
+++ roundcube/src/test/webapp/frontend/routes.js
@@ -2,7 +2,28 @@ describe('routes', function() {
   it("refresh setting", function() {
     Roundcube.routes();
   });
-  it("refresh setting", function() {
-    Roundcube.routes();
+  it("refresh - OC 6", function() {
+    OC.currentUser = "admin";
+    Roundcube.refreshInterval = 1;
+    OC.Router = "";
+    OC.Router.registerLoadedCallback = function(func) {
+    };
+    var routesSet = Roundcube.routes();
+    expect(routesSet).toBe(true);
+
+  });
+  it("refresh - OC 7", function() {
+    OC.currentUser = "admin";
+    Roundcube.refreshInterval = 1;
+    OC.Router = false;
+    var routesSet = Roundcube.routes();
+    expect(routesSet).toBe(true);
+  });
+  it("clear", function() {
+    OC.currentUser = false;
+    Roundcube.refreshInterval = 1;
+    OC.Router = false;
+    var routesSet = Roundcube.routes();
+    expect(routesSet).toBe(false);
   });
 });
