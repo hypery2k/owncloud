d350d3863d592fd9663c1720d3b3b2416236d16e
Bernhard Froehler <bernhard@bfroehler.info>
2014-08-29 09:35:26 +0200

    Improve feedback on storing Admin settings (fixes #266)

--- roundcube/src/main/php/js/adminSettings.js
+++ roundcube/src/main/php/js/adminSettings.js
@@ -15,14 +15,19 @@ Roundcube.adminSettingsUI = function() {
 
       var self = $(this);
       var post = $('#rcMailAdminPrefs').serialize();
+      $('#adminmail_update_message').html('<span class="adminmail_msg_status">Saving...</span>');
+      $('#adminmail_update_message').show();
 
       // Ajax foobar
       $.post(OC.filePath('roundcube', 'ajax', 'adminSettings.php'), post, function(data) {
 	if (data.status == 'success') {
-	  $('#adminmail_update_message').html(data.data.message);
+	  $('#adminmail_update_message').html('<span class="adminmail_msg_success">'+data.data.message+'</span>');
 	  $('#adminmail_update_message').show();
+	  window.setTimeout(function() {
+              $('#adminmail_update_message').hide();
+	  }, 10000);
 	} else {
-	  $('#adminmail_update_message').html(data.data.message);
+	  $('#adminmail_update_message').html('<span class="adminmail_msg_error">'+data.data.message+'</span>');
 	  $('#adminmail_update_message').show();
 	}
       }, 'json');
