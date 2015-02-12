85bbf525e4ee80423bab005805e98c5dab3056da
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-02-12 21:56:21 +0100

    Fix redirect error
    see #301

--- roundcube/src/main/php/appinfo/routes.php
+++ roundcube/src/main/php/appinfo/routes.php
@@ -3,3 +3,8 @@
 OC::$CLASSPATH['OC_RoundCube_AuthHelper'] = OC_App::getAppPath('roundcube') . '/lib/RoundCubeAuthHelper.class.php';
 
 $this->create('roundcube_refresh', '/refresh')->post()->action('OC_RoundCube_AuthHelper', 'refresh');
+$this->create('roundcube_index', '/')->action(
+		function($params){
+			require '/../index.php';
+		}
+);
