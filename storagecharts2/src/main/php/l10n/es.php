064a80f9013c9419979b425bbbc9609d3523579f
PICCORO Lenz McKAY <mckaygerhard@gmail.com>
2015-03-02 09:40:35 -0430

    Added translation spanish and russian for storagechars2

---
+++ storagecharts2/src/main/php/l10n/es.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+* ownCloud - DjazzLab Storage Charts plugin
+*
+* @author Xavier Beurois
+* @copyright 2012 Xavier Beurois www.djazz-lab.net
+* @copyright 2015 Martin Reinhardt martinreinhardt-online.de
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either 
+* version 3 of the License, or any later version.
+* 
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*  
+* You should have received a copy of the GNU Lesser General Public 
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+* 
+* Spanish texts by PICCORO McKAY Lenz
+* 
+*/
+
+$TRANSLATIONS = Array(
+	'all users' => 'todos',
+	'April' => 'Abril',
+	'August' => 'Agosto',
+	'Average used space' => 'Promedio de espacio',
+	'December' => 'Diciembre',
+	'February' => 'Febrero',
+	'January' => 'Enero',
+	'July' => 'Julio',
+	'June' => 'Junio',
+	'March' => 'Marzo',
+	'May' => 'Mayo',
+	'November' => 'Noviembre',
+	'October' => 'Octubre',
+	'September' => 'Septiembre',
+	'Monthly Used Space Evolution' => 'Evolucion de espacio mensual',
+	'Last 7 days' => 'Ultimos 7 dias',
+	'Daily Used Space Evolution' => 'Evolucion de espacio diario',
+	'Current ratio free space / used space' => 'Relacion de espacio libre / usado',
+	'for' => 'para',
+	'Free space' => 'Spacio libre',
+	'Drag\'N\'Drop on the chart title to re-order' => 'Arrastra\'Y\'Suelta los titulos para reubicar',
+	'Gigabytes (GB)' => 'Gigabytes (GB)',
+	'Kilobytes (KB)' => 'Kilobytes (KB)',
+	'Megabytes (MB)' => 'Megabytes (MB)',
+	'Terabytes (TB)' => 'Terabytes (TB)',
+	'Used space' => 'Espacio usado',
+	
+	// Personal settings
+	'Save' => 'Guardar',
+	'Save OK' => 'Guardado OK',
+	'Uncheck charts you do not want to display' => 'Descelecciona estatisticas que no quieras mostrar'
+);
---
+++ storagecharts2/src/main/php/l10n/ru.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+* ownCloud - DjazzLab Storage Charts plugin
+*
+* @author Xavier Beurois
+* @copyright 2012 Xavier Beurois www.djazz-lab.net
+* @copyright 2015 Martin Reinhardt martinreinhardt-online.de
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either 
+* version 3 of the License, or any later version.
+* 
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*  
+* You should have received a copy of the GNU Lesser General Public 
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+* 
+* Russnik(russian) texts by PICCORO McKAY Lenz
+* 
+*/
+
+$TRANSLATIONS = Array(
+	'all users' => 'все',
+	'April' => 'Апрель',
+	'August' => 'Август',
+	'Average used space' => 'Средний пространство',
+	'December' => 'Декабрь',
+	'February' => 'Февраль',
+	'January' => 'Январь',
+	'July' => 'Июль',
+	'June' => 'Июнь',
+	'March' => 'Март',
+	'May' => 'Май',
+	'November' => 'Ноябрь',
+	'October' => 'Октябрь',
+	'September' => 'Сентябрь',
+	'Monthly Used Space Evolution' => 'Месячная динамика пространство',
+	'Last 7 days' => 'Ultimos 7 dias',
+	'Daily Used Space Evolution' => 'Ежедневно используемых пространство',
+	'Current ratio free space / used space' => 'Соотношение текущем пространстве/используемый',
+	'for' => 'для',
+	'Free space' => 'Предлагаемая площадь',
+	'Drag\'N\'Drop on the chart title to re-order' => 'Перетаскивание чтобы изменить порядок',
+	'Gigabytes (GB)' => 'гигабайт (GB)',
+	'Kilobytes (KB)' => 'килобайт (KB)',
+	'Megabytes (MB)' => 'мегабайт (MB)',
+	'Terabytes (TB)' => 'терабайт (TB)',
+	'Used space' => 'Занятое пространство',
+	
+	// Personal settings
+	'Save' => 'Сохранить',
+	'Save OK' => 'Сохранить OK',
+	'Uncheck charts you do not want to display' => 'Снимите флажки диаграммы которые которые Вы не хотите чтобы отобразить'
+);
