4e5b3dc79730468e6cac67a9b0424d3b5e76eef3
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-03-12 01:11:02 +0100

    Corrected docker setup

--- webtest/src/main/docker/apps/bootstrap.sh
+++ webtest/src/main/docker/apps/bootstrap.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+java -jar /root/mockimap.jar --silent  &
+/etc/init.d/php5-fpm restart  &
+/etc/init.d/nginx restart  &
+/etc/init.d/mysql restart  &
+
 tail -F /var/log/nginx/*.log &
-tail -F /var/www/owncloud/data/owncloud.log &
+tail -F /var/www/owncloud/data/owncloud.log
