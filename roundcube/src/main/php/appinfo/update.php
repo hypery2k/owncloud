c99372e3814ab16c4676714cc243b907936406e3
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-03-31 20:47:59 +0200

    Fix update error
    remove build number from app version, see #314

--- roundcube/src/main/php/appinfo/update.php
+++ roundcube/src/main/php/appinfo/update.php
@@ -3,7 +3,7 @@
 //update from old app version, cleaning up old parameters
 
 $installedVersion = OCP\Config::getAppValue('roundcube', 'installed_version');
-if (version_compare($installedVersion, '2.5.4', '<')) {
+if (version_compare($installedVersion, '2.6.0', '<')) {
 	try {
 		$sql = 'DELETE FROM `*PREFIX*appconfig` WHERE appid = roundcube AND configkey = rcNoCronRefresh';
 		$args = array(1);
